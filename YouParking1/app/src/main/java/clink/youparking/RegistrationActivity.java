581bc8e34b9b631e1ebcc5ea474c36b0da28897b
YouParking1 <tclinkscales@troy.edu>
2016-09-14 17:38:08 -0500

    Autocomplete on universitys in registration

--- YouParking1/app/src/main/java/clink/youparking/RegistrationActivity.java
+++ YouParking1/app/src/main/java/clink/youparking/RegistrationActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+
+import java.util.Arrays;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -11,6 +16,13 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.universities, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.reg_university);
+        textView.setAdapter(adapter);
     }
 
     public void goToVehicleRegistration(View view)
