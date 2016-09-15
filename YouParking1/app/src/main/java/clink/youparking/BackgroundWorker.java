f15ce70b46697aee20b021fd24a27c6fcee9d8d9
SoumithThumma <soumith99@hotmail.com>
2016-09-15 17:00:24 -0500

    Inserting User Details into Database

--- YouParking1/app/src/main/java/clink/youparking/BackgroundWorker.java
+++ YouParking1/app/src/main/java/clink/youparking/BackgroundWorker.java
@@ -69,6 +69,49 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 e.printStackTrace();
             }
         }
+        else if (type.equals("register"))  {
+            String login_url = "http://www.troyparking.com/register.php";
+            showAlert = true;
+            try {
+                String FName = params[1];
+                String LName = params[2];
+                String University = params[3];
+                String Email = params[4];
+                String Password = params[5];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("Email", "UTF-8")+"="+URLEncoder.encode(Email, "UTF-8")+"&"
+                        +URLEncoder.encode("Password", "UTF-8")+"="+URLEncoder.encode(Password, "UTF-8")
+                        +"&"+URLEncoder.encode("FName", "UTF-8")+"="+URLEncoder.encode(FName, "UTF-8")
+                        +"&"+URLEncoder.encode("LName", "UTF-8")+"="+URLEncoder.encode(LName, "UTF-8")
+                        +"&"+URLEncoder.encode("University", "UTF-8")+"="+URLEncoder.encode(University, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line;
+                while((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         else if (type.equals("hold")) { //Send email, points, car, latitude, longitude, comments, and school
             try {
                 String hold_url = "http://www.troyparking.com/holdspot.php";
--- YouParking1/app/src/main/java/clink/youparking/RegistrationActivity.java
+++ YouParking1/app/src/main/java/clink/youparking/RegistrationActivity.java
@@ -1,31 +1,80 @@
 package clink.youparking;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity implements AsyncResponse {
+
+    EditText FName, LName, Email, Password, ConfirmPass;
+    AutoCompleteTextView University;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        FName = (EditText) findViewById(R.id.reg_first_name);
+        LName = (EditText) findViewById(R.id.reg_last_name);
+        Email = (EditText) findViewById(R.id.reg_email);
+        Password = (EditText) findViewById(R.id.reg_password);
+        ConfirmPass = (EditText) findViewById(R.id.reg_confirm_password);
+        University = (AutoCompleteTextView) findViewById(R.id.reg_university);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.universities, R.layout.dropdown_list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        AutoCompleteTextView textView = (AutoCompleteTextView)
-                findViewById(R.id.reg_university);
-        textView.setAdapter(adapter);
+
+        University.setAdapter(adapter);
+
     }
 
     public void goToVehicleRegistration(View view)
     {
-        Intent intent = new Intent(this, VehicleRegistrationActivity.class);
-        startActivity(intent);
+
+        String firstName = FName.getText().toString();
+        String lastName = LName.getText().toString();
+        String email = Email.getText().toString();
+        String password = Password.getText().toString();
+        String confirmpassword = ConfirmPass.getText().toString();
+        String university = University.getText().toString();
+
+        {
+            Context context = getApplicationContext();
+            CharSequence text = "Passwords do not match!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else if (!(email.toUpperCase().contains(".EDU") && email.contains("@"))){
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else {
+            String type = "register";
+            BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+            backgroundWorker.delegate = this;
+            backgroundWorker.execute(type, firstName,lastName, university,
+                        email,password);
+        }
+
+    }
+
+    @Override
+    public void processFinish(String output) {
+        if (output.contains("success")) {
+            Intent intent = new Intent(this, VehicleRegistrationActivity.class);
+            startActivity(intent);
+        }
     }
 }
 
