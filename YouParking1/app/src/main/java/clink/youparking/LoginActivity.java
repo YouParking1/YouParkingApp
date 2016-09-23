1656fc0ef1702718038bce19e2d85605bf8c24b1
YouParking1 <ParkingApp1>
2016-09-22 23:37:33 -0500

    Testing... ignore

--- YouParking1/app/src/main/java/clink/youparking/LoginActivity.java
+++ YouParking1/app/src/main/java/clink/youparking/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity implements AsyncResponse {
             User.lName = lName;
             User.school = school;
             Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            startActivity(intent); //DELETE THIS COMMENT
         }
 
         super.onCreate(savedInstanceState);
